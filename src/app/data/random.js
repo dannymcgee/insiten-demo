558a00a43b331af412b335f72fb78be61b04fa6b
dannymcgee <dannymcgee@gmail.com>
2019-07-03 00:46:26 -0400

    Added coinFlip and change methods

--- src/app/data/random.js
+++ src/app/data/random.js
@@ -3,7 +3,15 @@ class Random {
 
   static range(min, max) {
     return Math.round(Math.max(min, Math.random() * max))
-  }
+	}
+
+	static coinFlip() {
+		return Math.random() < 0.5
+	}
+
+	static chance(percent) {
+		return Math.random() < percent
+	}
 }
 
 module.exports = Random
