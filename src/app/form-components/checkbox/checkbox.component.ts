8d4684407f8ab4c12c11f003531469ce2dd47d97
dannymcgee <dannymcgee@gmail.com>
2019-07-04 15:04:41 -0400

    Added ternary mode

--- src/app/form-components/checkbox/checkbox.component.ts
+++ src/app/form-components/checkbox/checkbox.component.ts
@@ -1,5 +1,18 @@
-import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import {
+	Component,
+	OnInit,
+	Input,
+	HostBinding,
+	ViewEncapsulation
+} from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Subject, Subscription } from 'rxjs';
+
+enum TernaryState {
+	False = -1,
+	Null = 0,
+	True = 1
+}
 
 @Component({
 	selector: 'app-checkbox',
@@ -11,12 +24,17 @@ export class CheckboxComponent implements OnInit {
 	@Input() id: string;
 	@Input() baseClass: string;
 	@Input() form: FormGroup;
-	@Input() defaultValue: boolean;
+	@Input() isTernary: boolean;
+	@Input() defaultValue: boolean | null;
 	@Input() onChangeFn: ($event: any) => void;
 	control: FormControl;
+	ternaryState = new Subject<TernaryState>();
+	ternarySub: Subscription;
+	ternaryStateLast: TernaryState;
 
 	@HostBinding('class.checkbox') _ = true;
-	@HostBinding('class.checkbox--checked') isChecked = this.defaultValue || false;
+	@HostBinding('class.checkbox--checked') isChecked = false;
+	@HostBinding('class.checkbox--false') isNegative = false;
 	@HostBinding('class.checkbox--focus') isFocused = false;
 
 	constructor() {}
@@ -24,14 +42,70 @@ export class CheckboxComponent implements OnInit {
 	ngOnInit() {
 		this.control = new FormControl(null);
 		this.form.addControl(this.id, this.control);
-		this.control.setValue(this.defaultValue || false);
+		this.ternarySub = this.ternaryState.subscribe(state =>
+			this.updateTernaryValues(state)
+		);
+
+		this.initializeValue();
+	}
+
+	initializeValue() {
+		if (this.isTernary) {
+			switch (this.defaultValue) {
+				case true:
+					this.ternaryState.next(TernaryState.True);
+					break;
+				case false:
+					this.ternaryState.next(TernaryState.False);
+					break;
+				default:
+					this.ternaryState.next(TernaryState.Null);
+			}
+		} else {
+			this.control.setValue(this.defaultValue || false);
+			this.isChecked = this.control.value;
+		}
 	}
 
 	onChanged($event: any) {
-		this.control.value ? (this.isChecked = true) : (this.isChecked = false);
+		this.toggleState($event);
 
 		if (typeof this.onChangeFn === 'function') {
 			this.onChangeFn($event);
+		}
+	}
+
+	toggleState($event: any) {
+		if (this.isTernary) {
+			$event.preventDefault();
+			if (this.ternaryStateLast === TernaryState.True) {
+				this.ternaryState.next(TernaryState.False);
+			} else {
+				this.ternaryState.next(this.ternaryStateLast + 1);
+			}
+		} else {
+			this.isChecked = this.control.value;
+		}
+	}
+
+	updateTernaryValues(state: TernaryState) {
+		this.ternaryStateLast = state;
+
+		switch (state) {
+			case TernaryState.True:
+				this.control.setValue(true);
+				this.isChecked = true;
+				this.isNegative = false;
+				break;
+			case TernaryState.False:
+				this.control.setValue(false);
+				this.isChecked = false;
+				this.isNegative = true;
+				break;
+			default:
+				this.control.setValue(null);
+				this.isChecked = false;
+				this.isNegative = false;
 		}
 	}
 
