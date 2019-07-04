b533566041fe1fe73790539c8026ccd4b7e9a119
dannymcgee <dannymcgee@gmail.com>
2019-07-04 02:38:33 -0400

    Added FontAwesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+	faThLarge,
+	faThList,
+	faSearch,
+	faUserChart
+} from '@fortawesome/pro-solid-svg-icons';
 import 'what-input';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+	constructor() {
+		library.add(faThLarge, faThList, faSearch, faUserChart);
+	}
+}
