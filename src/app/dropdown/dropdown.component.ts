42a441d64aece6ab050d3048ef1b6ed730afa2bc
dannymcgee <dannymcgee@gmail.com>
2019-07-05 18:01:04 -0400

    minor CSS refactor

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -5,13 +5,14 @@ import {
 	Input,
 	ElementRef,
 	HostListener,
-	ViewChild
+	ViewEncapsulation
 } from '@angular/core';
 
 @Component({
 	selector: 'app-dropdown',
 	templateUrl: './dropdown.component.html',
-	styleUrls: ['./dropdown.component.scss']
+	styleUrls: ['./dropdown.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class DropdownComponent implements OnInit {
 	@Input() label: string;
