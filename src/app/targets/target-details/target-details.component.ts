2b5c254b26f93afa2abdc396ae5fc2b1883d617e
dannymcgee <dannymcgee@gmail.com>
2019-07-07 08:56:39 -0400

    Enabled modal closing and added fade transitions

--- src/app/targets/target-details/target-details.component.ts
+++ src/app/targets/target-details/target-details.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+	Component,
+	OnInit,
+	Input,
+	HostBinding,
+	ViewChild,
+	Renderer2,
+	AfterViewInit,
+	ElementRef
+} from '@angular/core';
 import {
 	Company,
 	Contact,
@@ -6,26 +15,44 @@ import {
 	DataManager
 } from 'src/app/data/data-manager.service';
 import { statusMap } from 'src/app/data/status.model';
+import { StateManager } from 'src/app/targets/state-manager.service';
 
 @Component({
 	selector: 'app-target-details',
 	templateUrl: './target-details.component.html',
 	styleUrls: ['./target-details.component.scss']
 })
-export class TargetDetailsComponent implements OnInit {
+export class TargetDetailsComponent implements OnInit, AfterViewInit {
 	@Input() company: Company;
 	status: { key: string; description: string; icon: string };
 	sDataManager = DataManager;
 	metricsMap = metricsMap;
 	metricKeys: string[];
-
 	locked = true;
 
-	constructor(public dataManager: DataManager) {}
+	@ViewChild('dialog', { static: false }) dialog: ElementRef;
+	@HostBinding('class.animate') _ = true;
+	@HostBinding('class.animate--fade-in') isOpening = true;
+	@HostBinding('class.animate--fade-out') isClosing = false;
+
+	constructor(
+		private dataManager: DataManager,
+		private stateManager: StateManager,
+		private renderer: Renderer2
+	) {}
 
 	ngOnInit() {
 		this.status = statusMap[this.company.status];
 		this.metricKeys = this.dataManager.getMetricKeys();
+
+		window.setTimeout(() => {
+			this.isOpening = false;
+		}, 300);
+	}
+
+	ngAfterViewInit() {
+		console.log('dialog type:', typeof this.dialog);
+		console.log('dialog:', this.dialog);
 	}
 
 	getSearchLink(contact: Contact) {
@@ -36,5 +63,21 @@ export class TargetDetailsComponent implements OnInit {
 		const queryStr = queryArr.join('+');
 
 		return `//google.com/search?q=${queryStr}`;
+	}
+
+	close() {
+		this.fadeOut();
+	}
+
+	fadeOut() {
+		this.isClosing = true;
+		this.renderer.removeClass(this.dialog.nativeElement, 'animate--fade-in-up');
+		this.renderer.removeClass(this.dialog.nativeElement, 'animate--slow');
+		this.renderer.addClass(this.dialog.nativeElement, 'animate--fast');
+		this.renderer.addClass(this.dialog.nativeElement, 'animate--fade-out-down');
+
+		window.setTimeout(() => {
+			this.stateManager.activeTarget.next(null);
+		}, 300);
 	}
 }
